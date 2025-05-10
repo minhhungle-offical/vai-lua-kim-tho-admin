792dfe2eefc783e562a1b1537c95d9c8babb46cf
minhhungle <minhhungle.offical@gmail.com>
2025-05-10 20:10:07 +0700

    last

--- src/components/Common/SideBar.jsx
+++ src/components/Common/SideBar.jsx
@@ -18,7 +18,7 @@ import {
 } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { NavLink } from 'react-router-dom'
-import logo from '@/assets/images/logo.png'
+import logo from '@/assets/images/logo.jpg'
 
 export default function SideBar({
   profile,
